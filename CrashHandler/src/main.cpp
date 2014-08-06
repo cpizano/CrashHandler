e366936bd250ce528956c696b8f21f8a4d07ec6a
Carlos Pizano <cpu@chromium.org>
2014-08-06 07:05:06 -0700

    Flesh out the server side
    Using one single thread with 60 events ready to be used.

--- CrashHandler/src/main.cpp
+++ CrashHandler/src/main.cpp
@@ -3,74 +3,149 @@
 #include <stdio.h>
 #include <tchar.h>
 #include <string>
+#include <vector>
+#include <array>
 
 #include <Windows.h>
+#include <DbgHelp.h>
 
 const wchar_t kPipeName[] = L"\\\\.\\pipe\\crash_srv_pipe";
 
-struct CrashInfoBlock {
-  char header[6];
+struct CrashRegistrationBlock {
+  char header[8];
   DWORD pid;
   DWORD tid;
-  int checksum;
-  EXCEPTION_RECORD* er;
+  MINIDUMP_EXCEPTION_INFORMATION* mex;
 
-  CrashInfoBlock() : pid(0), tid(0), checksum(0), er(nullptr) {
-    memcpy(header, "dump10", sizeof(header));
+  CrashRegistrationBlock()
+      : pid(0), tid(0), mex(nullptr) {
+    memcpy(header, "dumpv100", sizeof(header));
     pid = ::GetCurrentProcessId();
   }
 };
 
-class CrashClient {
-  volatile HANDLE pipe_;
-  CrashInfoBlock* cib_;
+struct CrashACKBlock {
+  char header[8];
+  HANDLE signal_event;
+  HANDLE wait_event;
 
+  CrashACKBlock()
+    : signal_event(NULL), wait_event(NULL) {
+    memcpy(header, "dumpack1", sizeof(header));
+  }
+};
+
+class CrashClient {
+  MINIDUMP_EXCEPTION_INFORMATION mexinfo_;
+  CrashRegistrationBlock crb_;
+  CrashACKBlock cab_;
+  
   static CrashClient* global_client;
 
 public:
-  CrashClient() : pipe_(0), cib_(nullptr) {
-    ::QueueUserWorkItem(&OpenPipe, this, 0);
-    cib_ = new CrashInfoBlock;
+  CrashClient() {
+    crb_.pid = ::GetCurrentProcessId();
+    crb_.tid = ::GetCurrentThreadId();
+    crb_.mex = &mexinfo_;
+    // Async registration.
+    ::QueueUserWorkItem(&Register, this, 0);
+    global_client = this;
   }
 
   CrashClient(const CrashClient&) = delete;
   
 private:
-  static DWORD __stdcall OpenPipe(void* ctx) {
+  static DWORD __stdcall Register(void* ctx) {
     CrashClient* client = reinterpret_cast< CrashClient*>(ctx);
-    HANDLE pipe = ::CreateFile(kPipeName,
-                               GENERIC_READ | GENERIC_WRITE, 0,
-                               NULL,
-                               OPEN_EXISTING, 0,
-                               NULL);
-    if (pipe != INVALID_HANDLE_VALUE) {
-      _InlineInterlockedExchangePointer(&client->pipe_, pipe);
-      _InlineInterlockedExchangePointer(reinterpret_cast<void**>(&global_client), client);
-      ::SetUnhandledExceptionFilter(ExHandler);
+    DWORD read = 0;
+    if (::CallNamedPipe(kPipeName,
+                        &client->crb_, sizeof(client->crb_),
+                        &client->cab_, sizeof(client->cab_),
+                        &read, 200000)) {
+      if (client->cab_.signal_event && client->cab_.wait_event)
+        ::SetUnhandledExceptionFilter(ExHandler);
     }
     return 0;
   }
 
-  static LONG __stdcall ExHandler(PEXCEPTION_POINTERS ex_info) {
-    CrashInfoBlock& cib = *global_client->cib_;
-    HANDLE pipe = global_client->pipe_;
-    cib.er = ex_info->ExceptionRecord;
-    cib.tid = ::GetCurrentThreadId();
-    DWORD written = 0;
-    if (::WriteFile(pipe, &cib, sizeof(cib), &written, NULL)) {
-      ::Sleep(200000);
-    }
+  static LONG __stdcall ExHandler(PEXCEPTION_POINTERS ex_ptrs) {
+    CrashACKBlock& cab = global_client->cab_;
+    CrashRegistrationBlock& crb = global_client->crb_;
+    crb.mex->ThreadId = ::GetCurrentThreadId();
+    crb.mex->ClientPointers = TRUE;
+    crb.mex->ExceptionPointers = ex_ptrs;
+
+    ::SignalObjectAndWait(cab.signal_event, cab.wait_event, 20000, FALSE);
     return EXCEPTION_EXECUTE_HANDLER;
   }
 };
 
 CrashClient* CrashClient::global_client = nullptr;
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+class EventThread {
+  static const int kEventCount = 60;
+  HANDLE events_[kEventCount];
+  size_t event_index_;
+
+  static DWORD __stdcall WaitRoutine(void* ctx) {
+    EventThread* event_thread = reinterpret_cast<EventThread*>(ctx);
+    while (true) {
+      DWORD wr = ::WaitForMultipleObjectsEx(
+          kEventCount, event_thread->events_, FALSE, INFINITE, TRUE);
+      if (wr < kEventCount)
+        event_thread->OnEvent(wr);
+      else if (wr == WAIT_IO_COMPLETION)
+        break;
+      else
+        __debugbreak();
+    }
+    return 0;
+  }
+
+public:
+  EventThread() : event_index_(0) {
+    for (int ix = 0; ix != kEventCount; ++ix) {
+      events_[ix] = ::CreateEvent(NULL, FALSE, FALSE, NULL);
+    }
+    ::CreateThread(NULL, 0, &WaitRoutine, this, 0, NULL);
+  }
+
+  HANDLE GetEvent(HANDLE process) {
+    HANDLE event = NULL;
+    if (!::DuplicateHandle(::GetCurrentProcess(), events_[event_index_],
+                           process, &event,
+                           SYNCHRONIZE, FALSE, 0))
+      return NULL;
+    event_index_++;
+    return event;
+  }
+
+  void OnEvent(size_t index) {
+
+  }
+
+};
+
 
 class CrashService {
   HANDLE pipe_;
 
+  struct ClientRecord {
+    HANDLE process;
+    HANDLE dump_done;
+    HANDLE dump_request;
+    DWORD pid;
+
+    ClientRecord(DWORD pid)
+        : process(NULL), dump_done(NULL), dump_request(NULL), pid(pid) {
+    }
+  };
+
+  EventThread event_thread_;
+  std::vector<ClientRecord> clients_;
+  
 public:
   CrashService() {
     pipe_ = ::CreateNamedPipe(kPipeName,
@@ -78,26 +153,58 @@ public:
         PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
         1,
         512, 512,
-        20,            
+        20,
         NULL);
   }
 
   void Run() {
-    ::ConnectNamedPipe(pipe_, NULL);
-    CrashInfoBlock cib;
-    DWORD read = 0;
-    ::ReadFile(pipe_, &cib, sizeof(cib), &read, NULL);
-    ::Beep(440, 40);
+    CrashRegistrationBlock crb;
+    while (true) {
+      while (true) {
+        if (!::ConnectNamedPipe(pipe_, NULL))
+          return;
+        DWORD read = 0;
+        if (!::ReadFile(pipe_, &crb, sizeof(crb), &read, NULL))
+          break;
+        if (read != sizeof(crb))
+          break;
+        if (crb.pid < 8)
+          break;
+        ClientRecord client(crb.pid);
+        client.process = ::OpenProcess(PROCESS_ALL_ACCESS, FALSE, crb.pid);
+        if (!client.process) {
+          if (!::ImpersonateNamedPipeClient(pipe_))
+            break;
+          client.process = ::OpenProcess(PROCESS_ALL_ACCESS, FALSE, crb.pid);
+          ::RevertToSelf();
+          if (!client.process)
+            break;
+        }
+        CrashACKBlock cab;
+        cab.signal_event = event_thread_.GetEvent(client.process);
+        client.dump_done = ::CreateEvent(NULL, TRUE, FALSE, NULL);
+        if (!::DuplicateHandle(::GetCurrentProcess(), client.dump_done,
+                               client.process, &cab.wait_event,
+                               SYNCHRONIZE, FALSE, 0))
+          break;
+        DWORD written = 0;
+        if (!::WriteFile(pipe_, &cab, sizeof(cab), &written, NULL))
+          break;
+        clients_.emplace_back(client);
+        break;
+      }
+      ::DisconnectNamedPipe(pipe_);
+    }
   }
 
   CrashService(const CrashService&) = delete;
 
 };
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 void DoEvenMoreWork(int x) {
-#if 0
+#if 1
   if (!x)
     __debugbreak();
 #endif
@@ -122,7 +229,7 @@ int Client() {
 int Server() {
   CrashService crash_service;
   crash_service.Run();
-
+  ::Sleep(200000);
   return 0;
 }
 
